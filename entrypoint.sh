c71fc8dba340f553ba0e8c48374fb7d1702b6703
Joao Guimaraes <38044115+0urob0r0s@users.noreply.github.com>
2019-05-14 13:00:18 -0300

    Updated - Fixed Rancher metadata gathering errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RANCHER_SERVICE_NAME=$(curl --retry 5 --retry-delay 5 --connect-timeout 3 -s http://rancher-metadata/2015-07-25/self/service/name)
+
 # Exeucte if deploying to Rancher
 if [ $RANCHER_SERVICE_NAME ] ; then
 
@@ -11,7 +13,7 @@ if [ $RANCHER_SERVICE_NAME ] ; then
   echo "STACK_NAME: ${STACK_NAME}"
 
   # Number of container instances running as part of service (i.e. scale)
-  SERVICE_INSTANCES=$(curl --retry 5 --retry-delay 5 --connect-timeout 3 -s http://rancher-metadata/2015-07-25/services/${RANCHER_SERVICE_NAME}/scale)
+  SERVICE_INSTANCES=$(curl --retry 5 --retry-delay 5 --connect-timeout 3 -s http://rancher-metadata/2015-07-25/services/${RANCHER_SERVICE_NAME}/containers | wc -l)
   echo "SERVICE_INSTANCES: ${SERVICE_INSTANCES}"
 
   # Set RabbitMQ hostname to equal that of Container name
